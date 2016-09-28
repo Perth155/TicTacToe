4f28605f365e8b429a02b39f61936dbaa272c2ad
Abrar <abrar.a.amin@gmail.com>
2016-09-28 22:51:43 +0800

    Add files via upload

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
 * Contains the main method used to run this game.
 * @author Abrar Amin
@@ -5,30 +6,108 @@
 
 public class TicTacToe
 {
+	
+	
 
 	public static void main(String[] args)
 	{
-
-		TicTacToeBoard newBoard = new TicTacToeBoard();
-		newBoard.drawBoard(); 
+		GamePlay game = new GamePlay();
+		game.getBoard().drawExpandedBoard();
+		Scanner s1 = new Scanner(System.in);
+		int select;
 		
-		while(newBoard.getCurrentState() == 0)
+		newGameStarter(game, s1);
+		
+		while(game.getCurrentState() == 0)
 		{
-			newBoard.validateSelection(newBoard.getPlayer1());
-			newBoard.drawBoard();
-			newBoard.terminateGame();
+			
+			select = getSelection(game, s1, game.getPlayer1());
+			System.out.println(game.getBoard().checkValidityOfMove(select));
+			while(!(game.getBoard().checkValidityOfMove(select)))
+			{
+				System.out.println("** Invalid selection by "+game.getPlayer1().getName()+".");
+				game.getBoard().drawExpandedBoard();
+				select = getSelection(game, s1, game.getPlayer1());
+			}
+			game.updateBoard(select, game.getPlayer1());
+			game.getBoard().drawBoard();
+			game.terminateGameCheck();
 
-			if(newBoard.isFull() || newBoard.getCurrentState() != 0) //break out of the while loop only if the game is over. 
+			
+			if(game.getBoard().isFull() || game.getCurrentState() != 0) //break out of the while loop only if the game is over. 
 				break;
 
-			newBoard.validateSelection(newBoard.getPlayer2());
-			newBoard.drawBoard();
-			newBoard.terminateGame();
+			
+			select = getSelection(game, s1, game.getPlayer2());
+			
+			while(! game.getBoard().checkValidityOfMove(select))
+			{
+				System.out.println("** Invalid selection by "+game.getPlayer2().getName()+".");
+				game.getBoard().drawExpandedBoard();
+				select = getSelection(game, s1, game.getPlayer2());
+			}
+			
+			game.updateBoard(select, game.getPlayer2());
+			game.getBoard().drawBoard();
+			game.terminateGameCheck();
 
 		}
 
 
-		newBoard.endGameResult();
+		game.endGameResult();
+		System.exit(0);
+	}
+	
+	
+	private static void newGameStarter(GamePlay game, Scanner inScan) 
+	{
+		System.out.println("Player 1's name: ");
+		String name1 = inScan.nextLine();
+		System.out.println("Player 2's name: ");
+		String name2 = inScan.nextLine();
+		
+		if(name1.equals(""))
+			name1 = "player1";
+		if(name2.equals(""))
+			name2 = "player2";
+		
+		game.setUpGame(name1, 'X', name2, 'O');
+	}
+
+
+	/**
+	 * Get the selection as an int between 1 to 8, to be passed to GamePlay object to validate. 
+	 * @param game
+	 * @param inScan
+	 */
+	private static int getSelection(GamePlay game, Scanner inScan, Player currentPlayer) 
+	{
+		int selection;
+		System.out.println(currentPlayer.getName()+"'s Turn.");
+		System.out.println("Enter an unoccupied slot between 0 and 8, or h for help.");
+		
+		
+		
+		String select = inScan.nextLine();
+		
+		while(select.equals("h") || select.equals("H"))
+		{
+			game.getBoard().drawExpandedBoard();
+			System.out.println("Enter an unoccupied slot between 0 and 8, or h for help.");
+			select = inScan.nextLine();
+		}
+		
+		try
+		{
+			selection = Integer.parseInt(select); // Now we need to check if the selection by the player is valid. Note str to int.
+		}
+		catch (NumberFormatException n)
+		{
+			selection = 100;
+		}
+		
+		
+		return  selection;	
 	}
 
 }
--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.util.Arrays;
 
 /**
@@ -14,17 +13,9 @@ public class TicTacToeBoard
 	private static final int ROW = 3; // Num of rows used in the 3*3 array of chars.
 	private static final int COL = 3; // Num of cols used in the 3*3 array of characters.
 	private static final char DEFAULT = '-'; // Default value to be stored in the array of chars.
-	private static final int RESUMEGAME = 0; 
-	private static final int TIE = 1; 
-	private static final int XWINS = 2; 
-	private static final int YWINS = 3; 
 	
-
-	private Player player1;
-	private Player player2;
 	private char[][] boardStatus; // 2D array 
 	private int playerSelect; // The integer input of the player.
-	private int currentState; // an int that contains the current state of game, used to determine game over.
 	
 	
 	/**
@@ -32,9 +23,6 @@ public class TicTacToeBoard
 	**/
 	public TicTacToeBoard()
 	{
-		player1 = new Player();
-		player2 = new Player("Player2", 'O');
-		currentState = 0;
 		boardStatus = new char[ROW][COL];
 		for(int i = 0; i<ROW; i++)
 		{ 
@@ -42,42 +30,10 @@ public class TicTacToeBoard
 		}
 	}
 
-
-	public Player getPlayer1()
+	// Accessor. 
+	public char[][] getBoardStatus() 
 	{
-		return player1; // Invokes the copy constructor of the Player Class.
-	}
-
-	public Player getPlayer2()
-	{
-		return player2;
-	}
-
-	public int getCurrentState()
-	{
-		return currentState;
-	}
-
-	/**
-	* A method checks if the entered input is a valid integer and the slot is not
-	* already occupied. 
-	* @param Player class object - the Player object that has made the move.
-	*/
-
-	public void validateSelection(Player inPlayer)
-	{	 
-		System.out.println(inPlayer.getName()+"'s Turn.");
-	 	System.out.println("Choose a free spot on the board from 0-8 to place a "+inPlayer.getSymbol()+".");
-	 	playerSelect = inPlayer.makeSelection();
-	 	boolean validSelection = checkValidityOfMove(playerSelect);
-	 	while(!validSelection)
-	 	{
-	 		System.out.println(playerSelect+" Was an invalid selection by " +inPlayer.getName()+".");
-	 		System.out.println("Please Choose a FREE spot between 0 to 8 only.");
-	 		playerSelect = inPlayer.makeSelection();
-	 		validSelection = checkValidityOfMove(playerSelect);
-	 	}
-	 	adjustBoardStatus(playerSelect, inPlayer.getSymbol());
+		return boardStatus;
 	}
 	
 	/**
@@ -97,6 +53,41 @@ public class TicTacToeBoard
 	* current boardStatus. 
 	*/
 	public void drawBoard()
+	{
+		//refresh(); // Clears the console
+		int count = 0;
+
+		System.out.print("\n ------tic-tac-toe------");  // Prints title of the game.
+		System.out.println(HEADER);
+
+		for(int i = 0; i<boardStatus.length; i++)
+		{
+			System.out.print(" | ");
+			for(int j = 0; j<boardStatus[i].length; j++)
+			{
+				if(boardStatus[i][j] == DEFAULT)
+				{
+					System.out.print("     | ");
+				}
+
+				else if(boardStatus[i][j] == 'X')
+				{
+					System.out.print("  "+'X'+"  | ");
+				}
+
+				else
+				{
+					System.out.print("  "+'O'+"  | ");
+				}
+				count++;
+			}
+			
+			System.out.println(HEADER);
+		}
+	}
+	
+	
+	public void drawExpandedBoard()
 	{
 		//refresh(); // Clears the console
 		int count = 0;
@@ -131,74 +122,6 @@ public class TicTacToeBoard
 	}
 
 
-	/**
-	* Checks if the game needs to be terminated.
-	* I.e. if one player wins the game. 
-	*/
-	public void terminateGame()
-	{
-		char[] simpleArray = new char[9]; // An 1D array representation of the 2D boardStatus array to compare diagonal. 
-		int count = 0; // Accumulating variable to populate simpleArray in the nested FOR loop.
-
-
-		for(int i = 0; i<ROW; i++)
-		{
-			// Check if a player has won horizontally eg. O-O-O.
-			if((boardStatus[i][0] == boardStatus[i][1]) && (boardStatus[i][0]== boardStatus[i][2]) && (boardStatus[i][0] != DEFAULT))
-			{
-				if(boardStatus[i][0] == 'X')
-					currentState = XWINS;
-				else
-					currentState = YWINS;
-			}
-			// Check if a player has won vertically. 
-			else if((boardStatus[0][i] == boardStatus[1][i]) && (boardStatus[0][i] == boardStatus[2][i]) && (boardStatus[0][i] != DEFAULT))
-			{
-				if(boardStatus[0][i] == 'X')
-					currentState = XWINS;
-				else
-					currentState = YWINS;
-			}
-			// Populate simpleArray with the rows and cols of the 2D array used for the board.
-			for(int j = 0; j<COL; j++)
-			{
-				simpleArray[count] = boardStatus[i][j];
-				count++;
-			}
-
-		}
-
-		diagonalWinCheck(simpleArray); // Check if a player won diagonally.
-	}
-
-
-	/* 
-	* Prints out the final outcome of the game to console.
-	*/
-	public void endGameResult()
-	{
-		if(currentState == XWINS)
-			System.out.println("**GAME OVER!\n**Player1 wins! Winner: "+player1.getSymbol());
-		else if(currentState == YWINS)
-			System.out.println("**GAME OVER!**\n**Player2 wins! Winner: "+player2.getSymbol());
-		else
-			System.out.println("**GAME OVER!**\n**Match Tied**");
-	}
-
-
-
-	public void diagonalWinCheck(char[] inArray)
-	{
-		if((inArray[4] != '-') && ((inArray[0] == inArray[4] && inArray[4] == inArray[8]) || (inArray[4] == inArray[2] && 
-			inArray[4] == inArray[6])))
-		{
-			if(boardStatus[1][1] == 'X')
-					currentState = XWINS;
-			else
-					currentState = YWINS;
-		}
-	}
-
 
 	/**
 	* Converts the console input from the user- an int between 0 to 9
@@ -229,4 +152,5 @@ public class TicTacToeBoard
 
 		return true;
 	}
+
 }
